4b0f4aef3ae5357b435fa65d8b06f76ad189cea2
Rob Carr <robert@inspire.scot>
2023-02-20 16:15:01 +0000

    Generic Document Thumbnail
    Formatter for Media > Documents to render a generic icon, rather than sample a PDF thumbnail
    
    Signed-off-by: Rob Carr <robert@inspire.scot>

---
+++ web/modules/custom/generic_thumbnail_formatter/src/Plugin/Field/FieldFormatter/GenericThumbnailFormatter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\generic_thumbnail_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
+
+/**
+ * 
+ * @FieldFormatter(
+ *   id = "generic_thumbnail_formatter_document",
+ *   label = @Translation("Standard Document thumbnail"),
+ *   field_types = {
+ *     "image"
+ *   }
+ * )
+ */
+class GenericThumbnailFormatter extends FormatterBase {
+
+  /**
+   * The viewElements function is where we're able to make modifications to
+   * the FieldItemListInterface variable $items.
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+      
+    // Initialize an array to store  processed items.
+    $elements = [];
+    $module_path = \Drupal::service('extension.list.module')->getPath('generic_thumbnail_formatter');
+    $image_path = '/' . $module_path . '/images/doc.png';
+
+    // In this for loop we'll take the value of each item, and then reverse the string.
+    foreach ($items as $delta => $item) {
+
+      //$filename = $item->value;
+      $markup = '<img src="' . $image_path . '" alt="Generic document icon">';
+      $elements[$delta] = ['#markup' => $markup];
+
+    }
+
+    // Lastly, we need to return the $elements array so it gets output for rendering.
+    
+    // ** option ** Consider outputting as image in render array which would
+    // allow custom styles
+    return $elements;
+  }
+
+}
