5acd4584d5fe5d7d9724dedde6841c98c5446399
Robert Carr <47385064+rgcarr@users.noreply.github.com>
2023-04-25 15:58:08 +0100

    Handle Array to String Error
    Could be related to lazy loading or caching. See https://drupal.stackexchange.com/questions/276840/attributearray-fails-to-implode-allowed-tags for more clues

--- web/core/lib/Drupal/Core/Template/AttributeArray.php
+++ web/core/lib/Drupal/Core/Template/AttributeArray.php
@@ -72,10 +72,21 @@ class AttributeArray extends AttributeValueBase implements \ArrayAccess, \Iterat
   /**
    * Implements the magic __toString() method.
    */
-  public function __toString() {
+   public function __toString() {
+    // Normalize value removing any invalid sub-array structures
+    $normalizedValue = [];
+    $bad = false;
+    foreach ($this->value as $k => $v) {
+      if (is_numeric($k) && is_string($v)) {
+        $normalizedValue[] = $v;
+      } else {
+        $bad = true;
+      }
+    }
+    
     // Filter out any empty values before printing.
-    $this->value = array_unique(array_filter($this->value));
-    return Html::escape(implode(' ', $this->value));
+    $this->value = array_unique(array_filter($normalizedValue));
+    return Html::escape(implode(' ', $normalizedValue));
   }
 
   /**
