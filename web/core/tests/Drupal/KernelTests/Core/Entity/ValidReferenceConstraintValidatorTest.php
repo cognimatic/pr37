21c01deec1aaec0aefe2f0bf65b92cb91b42f982
Rob Carr <robert@inspire.scot>
2023-07-03 17:23:30 +0100

    Manual refresh of Drupal Core - lots of info.yml
    Signed-off-by: Rob Carr <robert@inspire.scot>

--- web/core/tests/Drupal/KernelTests/Core/Entity/ValidReferenceConstraintValidatorTest.php
+++ web/core/tests/Drupal/KernelTests/Core/Entity/ValidReferenceConstraintValidatorTest.php
@@ -162,7 +162,11 @@ class ValidReferenceConstraintValidatorTest extends EntityKernelTestBase {
     $this->container->get('account_switcher')->switchTo($user_without_access);
 
     $violations = $referencing_entity->field_test->validate();
-    $this->assertCount(0, $violations);
+    $this->assertCount(1, $violations);
+    $this->assertEquals(t('This entity (%type: %id) cannot be referenced.', [
+      '%type' => 'node',
+      '%id' => $unpublished_node->id(),
+    ]), $violations[0]->getMessage());
 
     // Now save the referencing entity which will create a pre-existing state
     // for it and repeat the checks. This time, the user without access should
